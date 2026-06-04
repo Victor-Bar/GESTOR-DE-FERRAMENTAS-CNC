c70147e0c25f6a5a9fa44f7c2c8bff5ef20a9435
Victor Baratta <vitinhopvp94@gmail.com>
2026-06-04 15:21:12 -0300

    Exclusao dos testes
    Os códigos anteriores foram feito para testes a partir de agora, os novos códigos commitados seram parte do nosso projeto

--- backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/auth.controller');
-
-// POST /auth/login — rota pública, não exige token
-router.post('/login', authController.login);
-
-module.exports = router;
