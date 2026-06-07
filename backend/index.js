f83fbb8d59e7cd9162b5e7f9d0c800c298997e9c
matheusmoraesdemattos-coder <matheusmoraesdemattos@gmail.com>
2026-06-07 11:01:35 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,13 @@ const app = express();
 
 const authRoutes = require('./routes/auth');
 const routerFerramentas = require('./routes/ferramentas');
+const usuariosRoutes = require('./routes/usuarios');
 
 app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/ferramentas', routerFerramentas);
-
+app.use('/usuarios', usuariosRoutes);
 
 app.listen(3000, () => {
     console.log("Servidor express executando na porta 3000\n http://localhost:3000");
